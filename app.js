5f3a497d4e4bd6eb64fc43fc584befddfb4a04f5
thormiwa <thormiwa04@gmail.com>
2022-06-20 00:14:01 +0100

    cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 
 
